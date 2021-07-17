8922b96befff8b7c809c5ed99a01472feb65dd6a
RanonSenpai <ranonsenpai@gmail.com>
2021-07-17 22:08:16 +0530

    Larry Connor Pullback Strategy complete!

--- Strategies/pullback_strategies.py
+++ Strategies/pullback_strategies.py
@@ -10,13 +10,14 @@ def safe_round(value, digits):
 
 class LarryConnorPullbackStrategy(strategy.BacktestingStrategy):
     # This is a strategy with 88.89% Winning Rate
-
-    def __init__(self, feed, instrument, capital):
-        super(LarryConnorPullbackStrategy, self).__init__(feed, capital)
+    def __init__(self, feed, instrument, starting_equity):
+        super(LarryConnorPullbackStrategy, self).__init__(feed, starting_equity)
         # Banks and Config
         self.__instrument = instrument
         self.__long_position = None
+        self.__long_position_bought_days = 0
         self.__short_position = None
+        self.__short_position_bought_days = 0
         # Indicators
         close_data_series = feed[instrument].getCloseDataSeries()
         self.__rsi10 = rsi.RSI(close_data_series, 10)
@@ -31,23 +32,38 @@ class LarryConnorPullbackStrategy(strategy.BacktestingStrategy):
 
     def exitLongSignal(self, bar):
         current_rsi_10 = safe_round(self.__rsi10[-1], 2)
-        signal = current_rsi_10 > 40
+        signal = current_rsi_10 > 40 or self.__long_position_bought_days >= 10
         return signal
 
     def enterShortSignal(self, bar):
-        return False
-
-    def exitShortSignal(self, bar):
-        return False
-
-    def onBars(self, bars):
-        # Indicators
         current_rsi_10 = safe_round(self.__rsi10[-1], 2)
         current_sma_200 = safe_round(self.__sma200[-1], 2)
-        # Waiting for enough bars to calculate RSI 10 and SMA 200
-        if current_rsi_10 is None or current_sma_200 is None:
-            return
+        current_price = bar.getPrice()
+        signal = current_rsi_10 > 40 and current_price > current_sma_200
+        return signal
 
+    def exitShortSignal(self, bar):
+        current_rsi_10 = safe_round(self.__rsi10[-1], 2)
+        signal = current_rsi_10 < 30 or self.__long_position_bought_days >= 10
+        return signal
+
+    def updateBoughtDays(self):
+        # Increasing bought days for long and short position if they exist
+        if self.__long_position is not None:
+            self.__long_position_bought_days += 1
+        else:
+            self.__long_position_bought_days = 0
+
+        if self.__short_position is not None:
+            self.__short_position_bought_days += 1
+        else:
+            self.__short_position_bought_days = 0
+
+    def onBars(self, bars):
+        self.updateBoughtDays()
+        # Waiting for enough bars to calculate RSI 10 and SMA 200
+        if self.__rsi10[-1] is None or self.__sma200[-1] is None:
+            return
         bar = bars[self.__instrument]
 
         # If long postion is not open, check if we should enter a long position
@@ -56,7 +72,7 @@ class LarryConnorPullbackStrategy(strategy.BacktestingStrategy):
                 shares = 10
                 self.__long_position = self.enterLong(self.__instrument, shares, True)
         # Check if we have to exit a long position
-        elif self.exitLongSignal(bar) and self.__long_position.exitActive():
+        elif self.exitLongSignal(bar) and not self.__long_position.exitActive():
             self.__long_position.exitMarket()
 
         # If short postion is not open, check if we should enter a short position
@@ -65,21 +81,31 @@ class LarryConnorPullbackStrategy(strategy.BacktestingStrategy):
                 shares = 10
                 self.__short_position = self.enterShort(self.__instrument, shares, True)
         # Check if we have to exit a long position
-        elif self.exitShortSignal(bar) and self.__long_position.exitActive():
+        elif self.exitShortSignal(bar) and not self.__short_position.exitActive():
             self.__short_position.exitMarket()
 
     def onEnterOk(self, position):
         execution_info = position.getEntryOrder().getExecutionInfo()
         order_price = execution_info.getPrice()
-        self.info("BUY at $%.2f" % order_price)
+        # Printing the logs based on the type of position
+        if position == self.__long_position:
+            self.info("BUY LONG POSITION at $%.2f" % order_price)
+        elif position == self.__short_position:
+            self.info("BUY SHORT POSITION at $%.2f" % order_price)
     
     def onEnterCanceled(self, position):
         self.__position = None
     
-    def onExitOk(self, postion):
-        execution_info = position.getEntryOrder().getExecutionInfo()
+    def onExitOk(self, position):
+        execution_info = position.getExitOrder().getExecutionInfo()
         order_price = execution_info.getPrice()
-        self.info("SELL at $%.2f" % order_price)
+        # Clearing long or short position from variables
+        if position == self.__long_position:
+            self.__long_position = None
+            self.info("SELL LONG POSTION at $%.2f" % order_price)
+        elif position == self.__short_position:
+            self.__short_position = None
+            self.info("SELL SHORT POSITION at $%.2f" % order_price)
 
     def onExitCanceled(self, position):
         # If the exit was canceled, re-submit it
--- main.py
+++ main.py
@@ -1,12 +1,39 @@
+from typing import final
 from pyalgotrade.barfeed import quandlfeed
 
 from Strategies.pullback_strategies import LarryConnorPullbackStrategy
 
 # Importing CSV as a bar feed
 feed = quandlfeed.Feed()
-feed.addBarsFromCSV("orcl", "Data/WIKI-ORCL-2000-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2000-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2001-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2002-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2003-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2004-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2005-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2006-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2007-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2008-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2009-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2010-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2011-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2012-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2013-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2014-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2015-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2016-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2017-quandl.csv")
+feed.addBarsFromCSV("orcl", "Data/Orcl/WIKI-ORCL-2018-quandl.csv")
 
 # Evaluating Strategy
-capital = 1000
-strategy = LarryConnorPullbackStrategy(feed, "orcl", capital)
+starting_equity = 1000
+strategy = LarryConnorPullbackStrategy(feed, "orcl", starting_equity)
 strategy.run()
+final_equity = strategy.getBroker().getEquity()
+
+print('''
+    2000 - 2018 (ORCL)
+    -------------------------
+    Starting Equity : %.2f
+    Final Equity : %.2f
+''' % (starting_equity, final_equity))
